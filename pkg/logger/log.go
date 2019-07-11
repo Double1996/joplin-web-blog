2d75385d02cf138df6ca480a0f2ef805567b24cc
双磊(double) <sl3@meitu.com>
2019-07-11 13:11:02 +0800

    [feature] logger mod

--- pkg/logger/log.go
+++ pkg/logger/log.go
@@ -1,17 +1,70 @@
 package logger
 
 import (
+	"github.com/gin-gonic/gin"
+	"os"
+	"sync"
 	"time"
 
 	"go.uber.org/zap"
 )
 
+var (
+	once      sync.Once
+	singleton *zap.Logger
+)
+
+func LogHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		path := c.Request.URL.Path
+		c.Next()
+		end := time.Now()
+		latency := end.Sub(start)
+		Info(path,
+			zap.Int("status", c.Writer.Status()),
+			zap.String("method", c.Request.Method),
+			zap.String("ip", c.ClientIP()),
+			zap.Duration("latency-ms", latency*1000),
+			zap.String("error", c.Errors.String()),
+		)
+	}
+}
+
+func newLogger(path string) (*zap.Logger, error) {
+	cfg := zap.NewProductionConfig()
+	// 如果环境为debug
+	if os.Getenv("IS_DEBUG") == "1" {
+		cfg.Level = zap.NewAtomicLevelAt(zap.DebugLevel)
+	}
+	// 指定输出路径，对应ES 一般为stdout
+	cfg.OutputPaths = []string{
+		path,
+	}
+
+	// 关闭采样
+	cfg.Sampling = nil
+	return cfg.Build()
+}
+
 func init() {
-	logger, _ := zap.NewDevelopment()
-	defer logger.Sync()
-	logger.Info("无法获取网址",
-		zap.String("url", "http://www.baidu.com"),
-		zap.Int("attempt", 3),
-		zap.Duration("backoff", time.Second),
-	)
+	once.Do(func() {
+		singleton, _ = newLogger(os.Getenv("LoggerPath"))
+	})
+}
+
+func Info(message string, field ...zap.Field) {
+	singleton.Info(message, field...)
+}
+
+func Debug(message string, field ...zap.Field) {
+	singleton.Debug(message, field...)
+}
+
+func Error(message string, fields ...zap.Field) {
+	singleton.Error(message, fields...)
+}
+
+func Fatal(message string, fields ...zap.Field) {
+	singleton.Fatal(message, fields...)
 }
